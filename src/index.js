cb3cfbf8728105e3b4440451f2fc65ce5b1aa599
Matt Goo <mattgoo@gmail.com>
2016-07-18 15:31:13 -0700

    update to index.js for react-router-redux push to work. Added logger

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers, bindActionCreators } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 
+import logger from 'src/middlewares/logger';
 import { AppContainer } from 'src/components/App.jsx!';
 import appReducer from 'src/reducers/app-reducer.js';
 
@@ -13,7 +14,7 @@ const store = createStore(
   combineReducers({
     routing: routerReducer,
     app: appReducer
-  }), applyMiddleware(thunk)
+  }), applyMiddleware(logger, thunk, routerMiddleware(browserHistory))
 );
 
 const history = syncHistoryWithStore(browserHistory, store);
